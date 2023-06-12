e519aca1d9a0a17b49c661a0115bbd123e8928ff
maanelll <manelarousse@gmail.com>
2023-06-12 14:32:46 +0200

    back-end user modifé

--- back-end/migrations/Version20230602122911.php
+++ back-end/migrations/Version20230602122911.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230531135825 extends AbstractMigration
+final class Version20230602122911 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -28,7 +28,7 @@ final class Version20230531135825 extends AbstractMigration
         $this->addSql('CREATE TABLE entreprise_localite (localite_id INT NOT NULL, entreprise_id INT NOT NULL, INDEX IDX_A707E22C924DD2B5 (localite_id), INDEX IDX_A707E22CA4AEAFEA (entreprise_id), PRIMARY KEY(localite_id, entreprise_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE offre_stage (id INT AUTO_INCREMENT NOT NULL, promotion_id INT DEFAULT NULL, duree_stag SMALLINT NOT NULL, base_renum DOUBLE PRECISION DEFAULT NULL, date_offre DATE NOT NULL, nb_places_offert SMALLINT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_955674F2139DF194 (promotion_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE promotion (id INT AUTO_INCREMENT NOT NULL, pilote_id INT DEFAULT NULL, promo VARCHAR(255) NOT NULL, INDEX IDX_C11D7DD1F510AAE9 (pilote_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, promotion_id INT DEFAULT NULL, centre_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D649139DF194 (promotion_id), INDEX IDX_8D93D649463CD7C3 (centre_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, promotion_id INT DEFAULT NULL, centre_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D649139DF194 (promotion_id), INDEX IDX_8D93D649463CD7C3 (centre_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE OffreStage_Competence ADD CONSTRAINT FK_8327FFD915761DAB FOREIGN KEY (competence_id) REFERENCES competence (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE OffreStage_Competence ADD CONSTRAINT FK_8327FFD9195A2A28 FOREIGN KEY (offre_stage_id) REFERENCES offre_stage (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE entreprise_localite ADD CONSTRAINT FK_A707E22C924DD2B5 FOREIGN KEY (localite_id) REFERENCES localite (id) ON DELETE CASCADE');
