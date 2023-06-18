50bf024ebe70dbfbe7f4c39ffac1b3b5d8c1574f
ayman0kallel <ayman0kallel@gmail.com>
2023-06-18 21:27:20 +0200

    fix #14: ajout snackbar au CRUD entreprise

--- front-end/src/config/constants.ts
+++ front-end/src/config/constants.ts
@@ -38,3 +38,31 @@ export const routes: RouteType = {
 
     }
 }
+export const SNACKBAR_MESSAGES = {
+  error: {
+    default: "Une erreur est survenue",
+    login: "Une erreur est survenu lors de la connexion",
+    axios: {
+      get: "Une erreur est survenue lors de la récupération des données",
+      post: "Une erreur est survenue lors de l'envoi des données",
+      patch: "Une erreur est survenue lors de la modification des données",
+      delete: "Une erreur est survenue lors de la suppression des données",
+      activeExpression: "Une expression est déjà active",
+      date: "La date de début est ppostérieur à la date de fin",
+    },
+  },
+  success: {
+    axios: {
+      post: "L'entité a été crée",
+      patch: "L'entité a été modifiée",
+      delete: "L'entité a été supprimée",
+    },
+  },
+}
+
+export const ALERTDIALOG_MESSAGES = {
+  delete: {
+    title: "Suppression de l'élément",
+    description: "Etes-vous sûr de vouloir supprimer cet élément ?",
+  },
+}
